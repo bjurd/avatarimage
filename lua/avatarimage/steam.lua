9e19afabed82dae774889528c15d692b3c14e60d
bjurd <177191469+bjurd@users.noreply.github.com>
2025-11-19 04:58:29 -0500

    Change to use miniprofiles
    These work around the restriction of private profiles giving jpg responses instead of gifs

--- lua/avatarimage/steam.lua
+++ lua/avatarimage/steam.lua
@@ -5,15 +5,45 @@ end
 
 Steam = Steam or {}
 
-Steam.ProfileURL = "https://steamcommunity.com/profiles/%s/"
+Steam.ProfileURL = "https://steamcommunity.com/miniprofile/%s/"
 Steam.DefaultAvatar = "https://avatars.fastly.steamstatic.com/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg"
-Steam.PatternDecor = [[<div class="playerAvatarAutoSizeInner".-<img%s+src="(https://[^"]-)"%s*>%s*</div>%s*</div>]] -- Avatar decorations add extra stuff inside
-Steam.PatternAvatar = [[<div class="playerAvatarAutoSizeInner".-<img%s+src="(https://[^"]-)"%s*>%s*</div>]]
+Steam.PatternAvatar = [[<div class="playersection_avatar%s[%s%S]-<img%s+[^>]-src="(https://[^"]-)"[^>]->]]
+
+--- @param SteamID64 string
+--- @return number|nil
+function Steam.SteamIDToCommunityID(SteamID64)
+	local SteamID = util.SteamIDFrom64(SteamID64)
+
+	if SteamID == "STEAM_0:0:0" then
+		print("bad conversion")
+		return nil
+	end
+
+	local Universe, ID, AccountID = string.match(SteamID, "^STEAM_(%d+):(%d+):(%d+)$")
+	if not Universe then print("no Universe", SteamID) return nil end
+
+	ID = tonumber(ID)
+	AccountID = tonumber(AccountID)
+
+	if not ID or not AccountID then
+		print("bad tonumber", ID, AccountID)
+		return nil
+	end
+
+	return (AccountID * 2) + ID
+end
 
 --- @param SteamID64 string
 --- @param Callback function
 function Steam.FetchProfile(SteamID64, Callback)
-	local ProfileURL = Format(Steam.ProfileURL, SteamID64)
+	local CommunityID = Steam.SteamIDToCommunityID(SteamID64)
+
+	if not CommunityID then
+		Callback(nil)
+		return
+	end
+
+	local ProfileURL = Format(Steam.ProfileURL, CommunityID)
 
 	http.Fetch(
 		ProfileURL,
@@ -47,9 +77,8 @@ function Steam.FetchAvatar(SteamID64, Callback)
 			return
 		end
 
-		local AvatarURL = string.match(Data, Steam.PatternDecor)
-		AvatarURL = AvatarURL or string.match(Data, Steam.PatternAvatar)
-
+		local AvatarURL = string.match(Data, Steam.PatternAvatar)
+		print("got", AvatarURL)
 		Callback(AvatarURL)
 	end)
 end
